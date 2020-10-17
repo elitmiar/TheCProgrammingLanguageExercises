9d2e08fde35eebebbc4384f0f2358a151d4afbd3
Roland Rieseberg <roland.rieseberg@snode.com>
2020-10-17 08:40:49 +0200

    Excercise 1

---
+++ Chaptor1/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // Include information about standard input output library
+
+int main()
+{
+	printf("hello, world\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("hello, ");
+	printf("world");
+	printf("\n");
+}
